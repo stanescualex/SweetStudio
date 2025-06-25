5e81a37001a086624b742372df56d68067e9bedd
Alexandru Stanescu <alexandru.stanescu@bitnovel.com>
2025-06-25 15:29:58 +0300

    change port

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 });
  
 // Start the server
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 
